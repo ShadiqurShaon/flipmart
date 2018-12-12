596e79ad0d036a0e289f86d4b14508cce936a344
shadiqurshaon <shadiqurshaon@gmail.com>
2018-12-13 00:34:58 +0600

    working on layouts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Navbar/>
+      <Sidebar/>
+      <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/layouts/NavBar'
+import Footer from './components/layouts/Footer'
+import Sidebar from './components/sidebar/Sidebar'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Navbar,
+    Footer,
+    Sidebar
   }
 }
 </script>
@@ -21,8 +26,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
